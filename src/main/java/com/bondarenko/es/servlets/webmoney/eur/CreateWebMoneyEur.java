323e116937b323de19f17b61d6d743184e68a571
Dima Bondarenko <d.bondarenko.1990@gmail.com>
2015-07-14 01:38:30 +0300

    Add servlets for user & wallets

--- src/main/java/com/bondarenko/es/servlets/webmoney/eur/CreateWebMoneyEur.java
+++ src/main/java/com/bondarenko/es/servlets/webmoney/eur/CreateWebMoneyEur.java
@@ -1,5 +1,10 @@
 package com.bondarenko.es.servlets.webmoney.eur;
 
+import com.bondarenko.es.model.User;
+import com.bondarenko.es.model.WebMoneyEUR;
+import com.bondarenko.es.service.UserServ;
+import com.bondarenko.es.service.WebMoneyEurServ;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,18 +14,30 @@ import java.io.IOException;
 import java.math.BigDecimal;
 
 /**
- * Created by Lil Wayne on 03.07.2015.
+ * Created by Lil Wayne on 13.07.2015.
  */
-@WebServlet(name = "CreateWebMoneyEur", urlPatterns = "/CreateWebMoneyEur")
+@WebServlet(name = "CreateWebMoneyEUR", urlPatterns = "/CreateWebMoneyEUR")
 public class CreateWebMoneyEur extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        long userId = Long.parseLong(request.getParameter("userId"));
-//        double tmpAmount = Double.parseDouble(request.getParameter("amount"));
-//        BigDecimal amount =
+        try {
+            long userId = Long.parseLong(request.getParameter("userId"));
+            WebMoneyEurServ webMoneyEurServ = new WebMoneyEurServ();
+            WebMoneyEUR webMoneyEUR = new WebMoneyEUR();
+            webMoneyEUR.setUserId(userId);
+            webMoneyEUR.setAmount(new BigDecimal(0));
+            webMoneyEurServ.create(webMoneyEUR);
+            UserServ userServ = new UserServ();
+            User user = userServ.read(userId);
+            user.setIsWebMoneyEUR(1);
+            userServ.update(user);
+            request.getRequestDispatcher("successful.jsp").forward(request, response);
+        }catch (Exception e){
+            request.getRequestDispatcher("notSuccessful.jsp").forward(request, response);
+        }
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doPost(request, response);
     }
 }
